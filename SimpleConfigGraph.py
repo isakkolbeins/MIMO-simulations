89c6b609de5df0fd9e12b8b3638a8758f553c47b
Ísak Arnar Kolbeins <isakkolbeins@gmail.com>
2023-12-13 16:14:45 +0100

    Typos and small fix

--- SimpleConfigGraph.py
+++ SimpleConfigGraph.py
@@ -3,17 +3,17 @@ import matplotlib.pyplot as plt
 
 SPEED_OF_LIGHT = 3e8
 
+
+
 class MIMOConfig:
     def __init__(self, num_antennas, receiver_angles, distance, frequency):
         
         # Initialize the MIMO config setting.
-
             # num_antennas (int): Number of transmitting antennas.
             # receiver_angles (list [degrees]): List of angles (in degrees) of receiver direction.
             # distance float: Distance between the transmitter and receivers.
             # frequency (float): The frequency of the signal
-    
-       
+           
         self.num_antennas = num_antennas
         # Number of receivers determined from the number of angles 
         self.num_receivers = len(receiver_angles)
@@ -41,30 +41,25 @@ class MIMOConfig:
 
 
 def plot_mimo_config(config, x_range=(-130, 130), y_range=(-30, 130)):
-
     # Show a plot of the MIMO configuration in 2D with positions of antennas and receivers.
 
     # Make it bigger
     plt.figure(figsize=(10, 6))
-
+   
     # Plot receivers
     plt.scatter(*zip(*config.receiver_positions), marker='o', label='Receivers', color='green')
-
+    
     # Plot antennas centered at (0,0)
     plt.scatter(*zip(*config.transmitter_antenna_positions), marker='^', label='Antennas', color='blue')
-
-    #antenna_positions = [pos[0] for pos in config.transmitter_antenna_positions    ]
-    #plt.scatter(antenna_positions, np.zeros_like(antenna_positions), marker='^', label='Antennas', color='blue')
-
+    
     # Plot the "transmitter"
     plt.scatter(0, 0, marker='s', color='red', label='Transmitter')
-
+    
     # Labeling the recivers 
     for i, (x, y) in enumerate(config.receiver_positions):
         plt.text(x, y+10, f'Receiver {i + 1}\n({config.receiver_angles[i]}°)', ha='center', va='center', bbox=dict(facecolor='white', alpha=0.5))
 
     # Finalize plot 
-
     plt.title(f'MIMO Configuration - { config.num_antennas } Antennas, { config.num_receivers } Receivers, Frequency: { config.frequency } Hz')
     plt.xlabel('X Distance (meters)')
     plt.ylabel('Y Distance (meters)')
@@ -76,5 +71,9 @@ def plot_mimo_config(config, x_range=(-130, 130), y_range=(-30, 130)):
 
 
 ### Testing 
+
 config = MIMOConfig(num_antennas=12, receiver_angles=[120, 90, 60], distance=100, frequency=3e9)
+plot_mimo_config(config)
+
+config = MIMOConfig(num_antennas=6, receiver_angles=[90], distance=100, frequency=3e9)
 plot_mimo_config(config)
